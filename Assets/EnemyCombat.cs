666cc6243eca2a61b026cc857439c4c39f2823c3
Paulius Ložys <paulius10@hotmail.lt>
2020-02-29 19:03:59 +0200

    Melee enemy attack
    https://cassette-studios.atlassian.net/jira/software/projects/CS/boards/1?selectedIssue=CS-34

---
+++ Assets/EnemyCombat.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCombat : MonoBehaviour
+{
+    public LayerMask playerMask;
+    public Transform attackPoint;
+
+    public float attackRange = 0.5f;
+
+    public float attackRate = 2; // attacks per second
+    float nextAttackTime = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var tmp = Physics2D.OverlapCircle(attackPoint.position, attackRange, playerMask);
+        
+        if (Time.time >= nextAttackTime)
+        {
+            if (tmp != null)
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        Debug.Log("Player hit");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint is null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
