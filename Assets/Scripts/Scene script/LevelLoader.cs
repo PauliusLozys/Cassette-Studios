e3aa7df214ad0268a613d3809fd4e2f04197a1be
RainbowDog98 <42966213+RainbowDog98@users.noreply.github.com>
2020-04-03 10:58:28 +0300

    Added scene transitions
    A player can now transition between scenes

---
+++ Assets/Scripts/Scene script/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public BoxCollider2D trigger;
+    public float transitionTime = 1f;
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+        //Wait for the animation to finish
+        yield return new WaitForSeconds(transitionTime);
+        //Load scene
+        SceneManager.LoadScene(levelIndex);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        LoadNextLevel();
+    }
+}
