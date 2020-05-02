452e60b529753675790458f12edefd9ce258fd1b
RainbowDog98 <42966213+RainbowDog98@users.noreply.github.com>
2020-05-02 15:09:20 +0300

    Added few levels and ability to go between them
    https://cassette-studios.atlassian.net/browse/CS-140

---
+++ Assets/Scripts/Scene script/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LevelManager
+{
+    /// <summary>
+    /// Indicates weather the player left the previous scene on the left side
+    /// </summary>
+    public static bool LeftOnLeft = false;
+
+
+
+}
---
+++ Assets/Scripts/Scene script/SpawningPlace.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpawningPlace : MonoBehaviour
+{
+    [SerializeField]
+    public bool IsLeft = false;
+
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!LevelManager.LeftOnLeft && IsLeft)
+        {
+            Debug.Log("Player entered on Left");
+            player.position = transform.position;
+        }
+        else if (LevelManager.LeftOnLeft && !IsLeft)
+        {
+            Debug.Log("Player entered on Right");
+            player.position = transform.position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (IsLeft)
+            {
+                Debug.Log("Player left on left");
+                LevelManager.LeftOnLeft = true;
+                // Go to the left scene
+
+                if(SceneManager.GetActiveScene().name != "Level 1")
+                    SceneManager.LoadScene("Level 1");
+            }
+            else
+            {
+                Debug.Log("Player left on right");
+                LevelManager.LeftOnLeft = false;
+                // Go to the right scene
+                if (SceneManager.GetActiveScene().name != "Level 2")
+                    SceneManager.LoadScene("Level 2");
+            }
+        }
+
+
+    }
+}
