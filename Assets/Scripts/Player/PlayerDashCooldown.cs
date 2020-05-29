abc473cffa34ce9f01e06b6daa609057dbda30e9
lukasdevenas <59114037+lukasdevenas@users.noreply.github.com>
2020-05-29 17:27:12 +0300

    Dash cooldown indicator
    https://cassette-studios.atlassian.net/browse/CS-179

---
+++ Assets/Scripts/Player/PlayerDashCooldown.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDashCooldown : MonoBehaviour
+{
+    public Image imageCooldown;
+    private float cooldownTime;
+    private PlayerController playerController;
+    private bool hasDashed;
+    private bool cooldownFinished;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        cooldownTime = playerController.dashCoolDown;
+        cooldownFinished = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hasDashed = playerController.GetDashStatus();
+
+        if (cooldownFinished && hasDashed)
+        {
+            cooldownFinished = false;
+        }
+
+        if (!cooldownFinished)
+        {
+            imageCooldown.fillAmount += 1 / cooldownTime * Time.deltaTime;
+
+            if (imageCooldown.fillAmount >= 1)
+            {
+                imageCooldown.fillAmount = 0;
+                cooldownFinished = true;
+            }
+        }
+    }
+}
