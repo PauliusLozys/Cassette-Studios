10d9d91fcbb956b97ef4382da8b9837fb621b11c
domgud <47145742+domgud@users.noreply.github.com>
2020-06-01 11:26:24 +0300

    Enemy touch damage
    Can no langer go through enemies without getting damaged

---
+++ Assets/Scripts/Enemy/EnemyDamageCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamageCheck : MonoBehaviour
+{
+    AttackDetails attackDetails;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        attackDetails.damageAmount = 30;
+        attackDetails.position = this.transform.position;
+        collision.transform.SendMessage("Damage", attackDetails);
+    }
+}
